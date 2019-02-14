6564c5ecec012ea50189f27a44d1a85ed564f58c
coraxyc <ycxing99@gmail.com>
2019-02-13 23:42:29 -0800

    finished lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,5 +26,18 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	console.log("url : /project/" +  idNumber);
+
+	
+	$.get('/project/' + idNumber, function(result) {
+
+		var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+			'<p>' + result['title'] + '</p>' +
+			'<p><small>' + result['date'] +
+			'</small></p>';
+	
+			$('#project' + idNumber + '  .details').html(projectHTML + result.summary);
+	});
+	
 	console.log("User clicked on project " + idNumber);
 }
